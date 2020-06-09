38c2b8eb66a0a3069a2050b329ee0fec214bdb80
junglast <junglast@daum.net>
2020-06-09 22:13:12 +0900

    200609 #2

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import os
 from .constants.directory import *
 
-# from .browser_automation.cafe24 import *
-# from .browser_automation.wconcept import *
+from .browser_automation.cafe24 import *
+from .browser_automation.wconcept import *
 
-# from .export_to_db.export_cafe24 import *
+from .export_to_db.export_cafe24 import *
 from .export_to_db.export_wconcept import *
